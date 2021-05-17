17cb8cf19dd7fc44336eb9ed08503c651fa64800
Twilii <gola1_20@uni.worc.ac.uk>
2021-05-17 17:44:02 +0100

    Adding a new Controller and View for displ;aying Database Information

--- FCC_ASPNC_WebApp/Controllers/CategoryController.cs
+++ FCC_ASPNC_WebApp/Controllers/CategoryController.cs
@@ -1,11 +1,41 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FCC_ASPNC_WebApp.Data;
+using FCC_ASPNC_WebApp.Models;
 
 namespace FCC_ASPNC_WebApp.Controllers
 {
-    public class CategoryController
+    /// <summary>
+    /// Controller for Category Table in SQLServer
+    /// </summary>
+    public class CategoryController : Controller
     {
+        //Create variable for db instance
+        private readonly ApplicationDbContext _db;
+
+        /// <summary>
+        /// uses inbuilt depedency injection in ASP to obtain db on creating the controller for use within the controller
+        /// </summary>
+        /// <param name="db">instance of db context</param>
+        public CategoryController(ApplicationDbContext db)
+        {
+            //assign db to instance variable
+            _db = db;
+        }
+
+
+
+        /// <summary>
+        /// return view for index action
+        /// </summary>
+        /// <returns>index view</returns>
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Categories;
+            return View(objList);
+        }
     }
 }
