986c9f6033172a244e7324a76515f1748dd4dd8d
Twilii <gola1_20@uni.worc.ac.uk>
2021-05-17 15:10:21 +0100

    Starting Project

---
+++ FCC_ASPNC_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FCC_ASPNC_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
