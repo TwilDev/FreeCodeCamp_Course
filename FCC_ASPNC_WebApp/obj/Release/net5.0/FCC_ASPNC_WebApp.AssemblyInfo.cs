4f38ec256698f6fa65e6944bb953e3b0dfac6476
Twilii <gola1_20@uni.worc.ac.uk>
2021-05-18 15:12:26 +0100

    Finalising and deployment to azure

--- FCC_ASPNC_WebApp/obj/Release/net5.0/FCC_ASPNC_WebApp.AssemblyInfo.cs
+++ FCC_ASPNC_WebApp/obj/Release/net5.0/FCC_ASPNC_WebApp.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("5eef058c-b64f-417e-9863-f80dd60acf5b")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("FCC_ASPNC_WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
---
+++ FCC_ASPNC_WebApp/obj/Release/net5.0/PubTmp/EFSQLScripts/FCC_ASPNC_WebApp.Data.ApplicationDbContext.sql
@@ -0,0 +1,70 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210517160720_addCategoryTableToDatabase')
+BEGIN
+    CREATE TABLE [Categories] (
+        [CategoryId] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NULL,
+        [DisplayOrder] int NOT NULL,
+        CONSTRAINT [PK_Categories] PRIMARY KEY ([CategoryId])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210517160720_addCategoryTableToDatabase')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210517160720_addCategoryTableToDatabase', N'5.0.6');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210517174451_ApplicationType')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210517174451_ApplicationType', N'5.0.6');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210517174958_AddApplicationTypeToDatabase')
+BEGIN
+    CREATE TABLE [ApplicationTypes] (
+        [ApplicationId] int NOT NULL IDENTITY,
+        [ApplicationName] nvarchar(max) NULL,
+        CONSTRAINT [PK_ApplicationTypes] PRIMARY KEY ([ApplicationId])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210517174958_AddApplicationTypeToDatabase')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210517174958_AddApplicationTypeToDatabase', N'5.0.6');
+END;
+GO
+
+COMMIT;
+GO
+
